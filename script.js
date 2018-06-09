b611cc804e8e87f51afd75406d32a6b327a614de
Ishan Dindorkar <ishan.dindorkar@yahoo.com>
2018-06-09 21:38:55 +0530

    Learned declaring variables ad how to use them

---
+++ script.js
@@ -0,0 +1,13 @@
+// declaring variables to store string value
+var firstName = "Ishan";
+var lastName = "Dindorkar"
+
+console.log("Hello ", firstName, lastName)
+
+// decalring variable to store numeric value
+var age = 30
+console.log("Your age is ", age)
+
+// decalring variable to store true or false
+var fullAge = true
+console.log("Is it your full age?", fullAge)
